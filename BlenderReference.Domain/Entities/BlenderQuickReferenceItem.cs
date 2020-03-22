f657d1a0d1631ba088abc537811b0ba63d7770a1
ORTIZFAMILY <ORTIZFAMILY@ORTIZFAMILY-PC>
2020-03-22 14:24:29 -0700

    Add project files.

---
+++ BlenderReference.Domain/Entities/BlenderQuickReferenceItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using BlenderReference.Domain.Enums;
+
+namespace BlenderReference.Domain
+{
+    public class BlenderQuickReferenceItem
+    {
+        public List<string> HotKeys { get; set; }        
+        public ModeTypeEnum ModeType { get; set; }
+        public bool CanGrabScaleRotate { get; set; }
+        public string Area { get; set; }
+
+        public string ConcatHotKeys()
+        {            
+            return string.Join("+", HotKeys); 
+        }
+
+    }
+}
---
+++ BlenderReference.Domain/Enums/ModeTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlenderReference.Domain.Enums
+{
+    public enum ModeTypeEnum
+    {
+        EditMode = 1,
+        ObjectMode = 2
+    }
+}
