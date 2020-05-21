65675b689c5e197d3bf6475c905ced7e45b6a963
ORTIZFAMILY <ORTIZFAMILY@ORTIZFAMILY-PC>
2020-05-21 12:23:08 -0700

    Updated Blender Menu Reference Item.

--- BlenderReference.Domain/Models/BlenderMenuReferenceItem.cs
+++ BlenderReference.Domain/Models/BlenderMenuReferenceItem.cs
@@ -6,14 +6,8 @@ namespace BlenderReference.Domain.Models
 {
     public class BlenderMenuReferenceItem
     {
-        public string Description { get; set; }
-        public string MenuStep1 { get; set; }
-        public string MenuStep2 { get; set; }
-        public string MenuStep3 { get; set; }
-        public string MenuStep4 { get; set; }
-        public string MenuStep5 { get; set; }
-        public string MenuStep6 { get; set; }
-        public string MenuStep7 { get; set; }
-        public string MenuStep8 { get; set; }
+        public string MenuItemName { get; set; }
+        public int SortOrderId { get; set; }
+
     }
 }
